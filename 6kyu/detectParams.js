a24997f5283832ab96ed4391e1848e8d9a39ac69
Zerro2003 <mugishaelie160@gmail.com>
2025-10-11 23:07:29 +0200

    this cata is for detecting params

---
+++ 6kyu/detectParams.js
@@ -0,0 +1,33 @@
+function isPangram(string) {
+  let chars = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let realString = string.split("");
+  let toLow = realString.map((b) => b.toLowerCase());
+  return chars.every((a) => toLow.includes(a));
+}
