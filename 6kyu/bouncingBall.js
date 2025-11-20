334d341f22190e2f769079e4924ce03de58a59a4
Zerro2003 <mugishaelie160@gmail.com>
2025-11-20 20:32:02 +0200

    the bouncing ball kata

---
+++ 6kyu/bouncingBall.js
@@ -0,0 +1,13 @@
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
+
+  let count = 1;
+  let height = h * bounce;
+
+  while (height > window) {
+    count += 2;
+    height *= bounce;
+  }
+
+  return count;
+}
