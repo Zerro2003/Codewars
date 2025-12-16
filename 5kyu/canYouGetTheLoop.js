b2fc191ba8319df67ad575ace8b14ce38dda91b0
Zerro2003 <mugishaelie160@gmail.com>
2025-12-16 19:33:45 +0200

    Adds a loop finding through linked list

---
+++ 5kyu/canYouGetTheLoop.js
@@ -0,0 +1,17 @@
+function loop_size(node) {
+  let slow = node;
+  let fast = node;
+  while (true) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) break;
+  }
+  let count = 1;
+  fast = fast.next;
+  while (slow !== fast) {
+    fast = fast.next;
+    count++;
+  }
+
+  return count;
+}
