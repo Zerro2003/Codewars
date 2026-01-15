21e8165d34af6be7f63eb6d6b5eb99f1285b6e1e
Zerro2003 <mugishaelie160@gmail.com>
2026-01-15 18:58:28 +0200

    feat: Add `splitString` function to solve a Codewars problem and configure VS Code to disable inline suggestions.

--- 6kyu/splitString.js
+++ 6kyu/splitString.js
@@ -4,6 +4,8 @@ function solution(str) {
   if (str.length % 2 !== 0) {
     str += "_";
   }
+
+  
   let result = [];
   for (let i = 0; i < str.length; i += 2) {
     result.push(str.slice(i, i + 2));
