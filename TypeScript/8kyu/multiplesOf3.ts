7fea6ab502f9c99609ac71183193f3f86c2d4168
Zerro2003 <mugishaelie160@gmail.com>
2026-03-02 20:26:35 +0200

    a challenge for multiples

---
+++ TypeScript/8kyu/multiplesOf3.ts
@@ -0,0 +1,13 @@
+export class Challenge {
+  static solution(num: number): number {
+    if (num < 0) return 0;
+
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
